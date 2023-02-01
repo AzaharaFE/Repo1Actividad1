6bde45f9713f77628875606d8d166a9499a88132
ALFONSOLDLS <alfonsoldls@gmail.com>
2023-02-01 18:11:10 +0100

    Creado método escribeNombre

---
+++ Primera_actividad_entornos/src/Alfonso/Alfonso.java
@@ -0,0 +1,16 @@
+package Alfonso;
+
+public class Alfonso {
+
+	public static void main(String[] args) {
+		
+		escribeNombre();
+
+	}
+
+	public static void escribeNombre() {
+		
+		System.out.println("Hola, soy Alfonso");
+		
+	}
+}
