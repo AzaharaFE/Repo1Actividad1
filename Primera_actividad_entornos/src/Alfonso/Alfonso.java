b70e84755a39eb4bb848512c8ea4fc39a71a9d8f
Azahara.Fonseca <azahara9013@hotmail.com>
2023-02-02 21:31:02 +0100

    añadidas 3 comidas en la clase Alfonso

--- Primera_actividad_entornos/src/Alfonso/Alfonso.java
+++ Primera_actividad_entornos/src/Alfonso/Alfonso.java
@@ -9,6 +9,7 @@ public class Alfonso {
 		
 		hobbies();
 		
+		comidas();
 	}
 	
 	public static void saludo(){
@@ -24,5 +25,13 @@ public class Alfonso {
 		System.out.println("3 HOBBIES : jugar a videojuegos, jugar al tenis, escuchar musica");
 	
 	}
+	public static void comidas(){
+
+		
+		System.out.println("3 COMIDAS: macarrones, pizza, hamburguesa");
+	
+	}
 
 }
+
+
