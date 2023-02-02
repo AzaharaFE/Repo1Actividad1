523528faf1d8296f83044bddfd0786e509b5d340
ALFONSOLDLS <alfonsoldls@gmail.com>
2023-02-02 20:43:43 +0100

    Añadido 3 hobbies

--- Primera_actividad_entornos/src/Alfonso/Alfonso.java
+++ Primera_actividad_entornos/src/Alfonso/Alfonso.java
@@ -6,6 +6,9 @@ public class Alfonso {
 	public static void main(String [] args) {
 		
 		saludo();
+		System.out.println("Jugar videojuegos");
+		System.out.println("Jugar tenis");
+		System.out.println("Escuchar musica");
 		
 	}
 	
