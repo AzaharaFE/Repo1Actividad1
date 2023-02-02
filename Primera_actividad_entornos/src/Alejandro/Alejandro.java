95fa746ac885bea3ff4482aad63dc9f75aef6dec
alejandro fernandez prieto <alejandro.mfp@live.com>
2023-02-02 20:32:38 +0100

    add 3 hobbies

--- Primera_actividad_entornos/src/Alejandro/Alejandro.java
+++ Primera_actividad_entornos/src/Alejandro/Alejandro.java
@@ -5,6 +5,10 @@ public class Alejandro {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola, soy Alejandro");
+		
+		System.out.println("Hobbie 1.- Ver fútbol");
+		System.out.println("Hobbie 2.- Ir al gimnasio");
+		System.out.println("Hobbie 3.- Hacer senderismo");
 
 	}
 
