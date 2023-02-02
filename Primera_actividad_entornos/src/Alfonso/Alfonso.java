d471e4d00f89f5a428701521d5d42d50f41f58fa
ALFONSOLDLS <alfonsoldls@gmail.com>
2023-02-02 19:44:58 +0100

    Añadido paquete y clase Alfonso

---
+++ Primera_actividad_entornos/src/Alfonso/Alfonso.java
@@ -0,0 +1,17 @@
+package Alfonso;
+
+public class Alfonso {
+
+	public static void main(String [] args) {
+		
+		saludo();
+		
+	}
+	
+public static void saludo(){
+		
+		System.out.println("Hola, soy Alfonso");
+		
+	}
+} 
+
